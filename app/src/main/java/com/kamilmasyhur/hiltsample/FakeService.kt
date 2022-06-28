be43b2db9d9983285c383d04b2a63b9478d27344
Ahmad Kamil Almasyhur <ahmad.almasyhur@gojek.com>
2022-06-28 20:24:02 +0700

    Add scope to FakeService to Singleton

--- app/src/main/java/com/kamilmasyhur/hiltsample/FakeService.kt
+++ app/src/main/java/com/kamilmasyhur/hiltsample/FakeService.kt
@@ -1,7 +1,9 @@
 package com.kamilmasyhur.hiltsample
 
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class FakeService @Inject constructor() {
     fun login(username: String, password: String): Boolean = true
 }
